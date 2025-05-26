b2c1a735e67bf2a1d8e54cd4d625267fa09daebb
victor olorunda <victorolorunda6@gmail.com>
2025-05-26 12:40:11 +0100

    added mailchimp script tag

--- src/app/author/[slug]/layout.tsx
+++ src/app/author/[slug]/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
         id="mcjs"
         strategy="beforeInteractive"
         dangerouslySetInnerHTML={{  __html: `
-        <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/3fea375d18ea12601b5d5c484/64aa59f1b085c7341cfe5e816.js");</script>
+       <script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/3fea375d18ea12601b5d5c484/2951018785749351a2c24dc65.js");</script>
       `}}/>
       </head>
       <body>
